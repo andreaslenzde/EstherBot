d676365bd74bcdf5e4b998ba9b827b4cccbdad4b
andreaslenzde <al@andreaslenz.de>
2016-05-25 12:56:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Sie wollen also mehr über Andreas Lenz erfahren? Schreiben Sie einfach HALLO um anzufangen.')
                 .then(() => 'speak');
         }
     },
